e43c928b17387ea8d2ee81e3f25ea4f9d1182b53
张益达 <7542491+peng9_zhang@user.noreply.gitee.com>
2021-02-17 21:14:55 +0800

    update router

---
+++ src/network/request.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+export function request() {
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:7878/api/m5'
+  })
+  instance.interceptors.request.use(config=>{
+    return config
+  },err=> {
+    console.log(err)
+  })
+//响应拦截
+  instance.interceptors.response.use(result=>{
+    return result.data
+  },err=> {
+    console.log(err)
+  })
+  return instance(config)
+}
+
